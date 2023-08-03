77d52a7af82745a860cbee61b6d63fd0a72d4411
NikhitaReddy16 <nikhita.reddy@rupeek.com>
2023-08-04 00:48:37 +0530

    second commit

--- src/components/customerComponent.vue
+++ src/components/customerComponent.vue
@@ -1,16 +1,55 @@
+
 <template>
   <div>
-    <h2>Customer Page</h2>
-    <p>Welcome to the Customer page!</p>
+    <div v-if="!isResponse">
+      <h2>Add Customer Details</h2>
+      <form @submit.prevent="addCustomer">
+        <label for="name">Name:</label>
+        <input type="text" id="name" v-model="customer.name" required>
+
+        <label for="age">Amount:</label>
+        <input type="number" id="age" v-model="customer.amount" required>
+
+        <label for="city">Gold:</label>
+        <input type="text" id="city" v-model="customer.gold" required>
+
+        <label for="city">Tenure:</label>
+        <input type="text" id="city" v-model="customer.tenure" required>
+
+        <button type="submit">Check my Schemes</button>
+      </form>
+    </div>
+    <div v-if="isResponse">
+    </div>
   </div>
 </template>
 
 <script>
+
+// import axios from 'axios';
 export default {
-  name: 'CustomerComponent'
+  name: 'AddCustomer',
+  data() {
+    return {
+      isResponse: false,
+      customer: {
+        name: '',
+        amount: null,
+        gold: null,
+        tenure: null
+      },
+    };
+  },
+  methods: {
+    async addCustomer() {
+      // Emit an event with the customer data to be handled by the parent component
+      // const response = await axios.post('https://api.example.com/customers', this.customer);
+      const response = 200;
+      if(response === 200){
+        this.isResponse = true;
+      }
+      console.log(this.customer.name);
+    },
+  },
 };
 </script>
-
-<style scoped>
-/* Add your component-specific styles here */
-</style>
